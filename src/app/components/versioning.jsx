e277c3a6590ee411853b876cf21e2a79a73d5391
mikaeruu <mchlbataller@gmail.com>
2020-04-14 23:32:08 +0800

    Added changelogs on landing page

--- src/app/components/versioning.jsx
+++ src/app/components/versioning.jsx
@@ -4,7 +4,7 @@ const Header = (props) => (
   <p className="text-2xl text-border-bottom"> {props.children} </p>
 );
 
-const Versioning = (props) => (
+const VersionList = (props) => (
   <div className="mt-12">
     <p className="text-3xl font-bold">Changelog</p>
     <div className="mx-20 mt-5 p-5 text-gray-900 bg-gray-300 font-mono rounded-md">
@@ -13,12 +13,12 @@ const Versioning = (props) => (
   </div>
 );
 
-const VersionList = (props) => (
-    <div>
-        <ul className="list-disc list-inside">
+const Version = (props) => (
+    <div className="mb-4">
+        <ul className="list-disc list-inside mx-5">
             {props.children}
         </ul>
     </div>
 )
 
-export { Header, Versioning, VersionList };
+export { Header, Version, VersionList };
--- src/app/pages/landing/Progress.jsx
+++ src/app/pages/landing/Progress.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Header, Versioning, VersionList } from 'app/components/versioning';
+import { Header, VersionList, Version } from 'app/components/versioning';
 
 const Progress = () => (
-    <Versioning>
-        <VersionList>
-            <Header>v1.0.0</Header>
-            <li> Test</li>
-        </VersionList>
-    </Versioning>
+    <VersionList>
+        <Version> <Header>v1.1</Header> <li> Integrated with Tailwind</li></Version>
+        <Version> <Header>v1.0</Header> <li> Added Authentication</li> </Version>
+    </VersionList>
 )
 
 export default Progress
