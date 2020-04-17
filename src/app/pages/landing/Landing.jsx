1e618967cff915dff146d3e9d4e185c93c43ecde
mikaeruu <mchlbataller@gmail.com>
2020-04-17 12:42:25 +0800

    Modified landing message

--- src/app/pages/landing/Landing.jsx
+++ src/app/pages/landing/Landing.jsx
@@ -3,12 +3,14 @@ import Progress from "./Progress";
 import React from "react";
 
 function Landing() {
-    return (
-        <div>
-            <h1 className="text-center text-3xl font-light">This app is my React playground.</h1> 
-            <Progress />
-        </div>
-    ) 
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-light">
+        This app is my coding playground.
+      </h1>
+      <Progress />
+    </div>
+  );
 }
 
 export default Landing;
