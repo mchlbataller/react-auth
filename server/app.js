e567005f89125291618799d2d26d4661c5a7e2cc
lenovo <mchlbataller@gmail.com>
2020-04-19 07:33:26 +0800

    Added passport functions

--- server/app.js
+++ server/app.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const db = require('./db');
 
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
+
+// Use Passport
+const passport = require('passport');
+
+// Local strategy by Passport
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user){
+      if(err) 
+        return cb(err);
+      if(!user)
+        return cb(null, false);
+      if(user.password != password)
+        return cb(null, false);
+      return cb(null, user);
+    })
+  }
+))
 
 const cors = require('cors');
 
 const auth = require('./src/endpoints/auth');
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 app.use(express.static(path.join(__dirname, 'routes')));
 app.use(cors());
+
 
 var schema = buildSchema(`
   type Query {
@@ -32,5 +57,12 @@ app.get('/', function(req, res) {
 app.get('/auth', function(req, res) {
     res.send(auth(req));
 })
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+      res.redirect('/');
+  }
+)
 
 app.listen(process.env.PORT || 9000)
---
+++ server/db/users.js
@@ -0,0 +1,21 @@
+exports.findById = function(id, cb) {
+    process.nextTick(function() {
+        var idx = id-1;
+        if(records[idx])    
+            cb(null, records[idx]);
+        else    // Make a new error showing that user does not exist
+            cb(new Error('User' + id + ' does not exist'));
+    })
+}
+
+exports.findByUsername = function(username, cb) {
+    process.nextTick(function() {
+        for(var i = 0, len = records.length; i < len; i++) {
+            var record = records[i];
+            if(record.username === username) {
+                return cb(null, record);
+            }
+        }
+        return cb(null, null);
+    })
+}
