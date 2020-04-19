283444718447b2fbce5fc1fc357d7e7d213b6872
lenovo <mchlbataller@gmail.com>
2020-04-19 13:54:56 +0800

    Cleanup of passport leftovers

--- server/app.js
+++ server/app.js
@@ -1,36 +1,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const db = require("./db");
-const Strategy = require("passport-local");
 
 const jwt = require("./src/services/jwt");
 
 const graphqlHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
 
-// Use Passport
-const passport = require("passport");
-
-// Local strategy by Passport
-passport.use(
-  new Strategy(function (username, password, cb) {
-    db.users.findByUsername(username, function (err, user) {
-      if (err) return cb(err);
-      if (!user) return cb(null, false);
-      if (user.password != password) return cb(null, false);
-      return cb(null, user);
-    });
-  })
-);
-
 const cors = require("cors");
 
 const auth = require("./src/endpoints/auth");
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use(express.static(path.join(__dirname, "routes")));
 app.use(cors());
 
