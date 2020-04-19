1f5e8e08d9dbf7de4c5af324e1b59cadb2b20752
lenovo <mchlbataller@gmail.com>
2020-04-19 10:12:57 +0800

    Reinstalled husky and lint-staged

--- server/app.js
+++ server/app.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
-const cors = require('cors');
+const cors = require("cors");
 
-const auth = require('./src/endpoints/auth');
+const auth = require("./src/endpoints/auth");
 
-app.use(express.static(path.join(__dirname, 'routes')));
+app.use(express.static(path.join(__dirname, "routes")));
 app.use(cors());
 
 var schema = buildSchema(` type Query { hello: String } `);
-var root = { hello: () => 'Hello world!' };
+var root = { hello: () => "Hello world!" };
 
-app.use('/graphql', graphqlHTTP({ schema: schema, rootValue: root, graphiql: true, }));
+app.use(
+  "/graphql",
+  graphqlHTTP({ schema: schema, rootValue: root, graphiql: true })
+);
 
-app.get('/', function(req, res) { res.send("Home"); }) 
+app.get("/", function (req, res) {
+  res.send("Home");
+});
 
-app.get('/auth', function(req, res) {
-    res.send(auth(req));
-})
+app.get("/auth", function (req, res) {
+  res.send(auth(req));
+});
 
-app.listen(process.env.PORT || 9000)
+app.listen(process.env.PORT || 9000);
