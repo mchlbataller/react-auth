dfe5fa930eefcb350f532280fc74f30b0d48e73e
mchlbataller <mchlbataller@gmail.com>
2020-04-28 18:21:34 +0800

    Installed prettier

--- app/src/app/pages/home/home.jsx
+++ app/src/app/pages/home/home.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import { H1 } from "app/components/text";
+
+function Hooks(props) {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    );
+}
 
 const Home = () => (
     <div>
-        <H1>I will practice Redux in this page.</H1>
+        <H1>I will practice Hooks in this page.</H1>
+        <Hooks />
     </div>
 );
 
