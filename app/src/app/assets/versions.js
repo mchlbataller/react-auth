36f3a008da2fd75e76e980419250d9199fa37a27
mchlbataller <mchlbataller@gmail.com>
2020-04-30 13:18:12 +0800

    Separated version records from Progress component

---
+++ app/src/app/assets/versions.js
@@ -0,0 +1,15 @@
+const versionRecords = [
+    {
+        version: 1.0,
+        notes: "Added authentication function using JSON Web Tokens",
+    },
+    {
+        version: 1.1,
+        notes: "Integrated app with Tailwind CSS, usage in components",
+    },
+    { version: 1.2, notes: "Revamped authentication page design" },
+    { version: 1.3, notes: "Enabled offline mode with service-workers" },
+    { version: 1.4, notes: "" },
+];
+
+export default versionRecords;
--- app/src/app/pages/landing/Progress.jsx
+++ app/src/app/pages/landing/Progress.jsx
@@ -1,19 +1,6 @@
 import React from "react";
 import { Header, VersionList, Version } from "app/components/versioning";
-
-const versionRecords = [
-    {
-        version: 1.0,
-        notes: "Added authentication function using JSON Web Tokens",
-    },
-    {
-        version: 1.1,
-        notes: "Integrated app with Tailwind CSS, usage in components",
-    },
-    { version: 1.2, notes: "Revamped authentication page design" },
-    { version: 1.3, notes: "Enabled offline mode with service-workers" },
-    { version: 1.4, notes: "" },
-];
+import versionRecords from "app/assets/versions";
 
 const Progress = () => (
     <VersionList>
